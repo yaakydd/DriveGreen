912c5c320b1262c757ef9d76e8dd1173c76d8fa7
Farouk <faroukpatterson4@gmail.com>
2025-12-20 03:48:36 +0000

    applied luminous ecoluxury redesign

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import PredictionForm from "./components/PredictionForm";
 import Chatbot from "./components/Chatbot";
+import BackgroundAurora from "./components/BackgroundAurora";
 import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
     <>
-      <PredictionForm />
+      <BackgroundAurora />
+      <main className="relative z-10 min-h-screen flex flex-col items-center justify-center p-4">
+        <PredictionForm />
+      </main>
       <Chatbot />
       <Toaster position="top-center" />
     </>
