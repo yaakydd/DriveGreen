c8a427435596dd100b3f357a2708324b00c4effc
Yaa <antwiyaa17@gmail.com>
2025-12-24 17:32:14 +0000

    Made changes to the PredictionForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import PredictionForm from "./components/PredictionForm";
 import Chatbot from "./components/Chatbot";
-import BackgroundParticles from "./components/BackgroundParticles";
-import NeonCar from "./components/NeonCar";
 import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
     <>
-      <div className="fixed inset-0 z-0 bg-slate-950 overflow-hidden pointer-events-none">
-        <BackgroundParticles />
-        <NeonCar />
-      </div>
-      <main className="relative z-10 min-h-screen w-full flex flex-col items-center justify-center py-8 px-4 md:p-6 lg:p-12 overflow-x-hidden">
-        <PredictionForm />
-      </main>
+      <PredictionForm />
       <Chatbot />
       <Toaster position="top-center" />
     </>
